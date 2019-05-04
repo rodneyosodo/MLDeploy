03a106d34cec2ef7ca864fafe960a7d256936bbe
Rodney Osodo <blackd0t@protonmail.com>
2019-05-04 18:53:22 +0300

    Adding example file

---
+++ src/example.py
@@ -0,0 +1,10 @@
+import Sentiment as s
+
+print(s.sentiment("This movie was awesome the acting was great, and there were pythons"))
+print(s.sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible movie. 0/10"))
+
+print(s.sentiment("Maize scandal"))
+print(s.sentiment("Trump signs order limiting migrant asylum at U.S.-Mexico border"))
+print(s.sentiment("There is awesome healthcare"))
+print(s.sentiment("Kibera houses are poor"))
+print(s.sentiment("This is Kenyatta University Referral  Hospital, Built at a cost of KSH 11 Billion, project commenced in 2014  and was completed in 2018. The hospital will ease pressure on @KNH_hospital and enhance capacity building of health workers in the region #Big4Agenda #KenyaMbele"))
